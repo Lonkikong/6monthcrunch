63307aee0ae9c3466846e17603a972a10fdce2a3
Liang <Liang@DESKTOP-H2H9IM4.home>
2018-05-05 10:56:37 +0200

    first commit 05052018

--- 6monthCrunch/src/test/Test.java
+++ 6monthCrunch/src/test/Test.java
@@ -1,8 +1,61 @@
 package test;
 
 public class Test {
-	public Test() {
-		System.out.println("Hello World");
-
-	}
+	
+	static String randomString = "String tp print";
+	
+	static final double PINUM = 3.141529;
+	public static void main(String[] args)
+	{
+		System.out.println("PINUM");
+		
+		int intergerOne = 22;
+				
+		int intergerTwo 
+		=
+		intergerOne
+		+22;
+		
+		System.out.println(intergerTwo);
+		
+		byte bigByte = 127;
+		short bigshort = 32767;
+		int bigInt = 210000;
+		long bigLong=9220000000000L;
+		float bigFloat = 3.14F;
+		double bigDouble = 3.14865548453648;
+		
+		System.out.println(Float.MAX_VALUE);
+		System.out.println(Double.MAX_VALUE);
+		
+		boolean trueOrFalse = true;
+		
+		char randomChar = 66;
+		char anotherChar= 'A';
+		
+		char escapedChars = '\\';
+		
+		String randomString = "Im a random string";
+		String anotherString = "Stuff";
+		
+		String andAnotherString = randomString + ' '+ anotherString;
+		
+		String byteString = Byte.toString(bigByte);
+		
+		byteString = Byte.toString(bigByte);
+		String shortString = Short.toString(bigshort);
+		String intString = Integer.toString(bigInt);
+		String longString = Long.toString(bigByte);
+		String floatString = Float.toString(bigByte);
+		String doubleString = Double.toString(bigByte);
+		
+		int aDoubleValue = (int) 3.1456466464;
+		int doubleToInt = (int) aDoubleValue;
+		
+		int stringToInt = Integer.parseInt(inString);
+		
+		parseShort, parselong, ParseByte, parseFloat, parseDouble, parseboolean;
+		System.out.println(doubleToInt);
+		
+}
 }
---
+++ 6monthCrunch/src/test/java2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class java2
+{
+	
+static Scanner userInput = new Scanner(System.in);
+
+public static void  main(String[] args)
+{
+	System.out.print("Your favorite Number: ");
+	
+	NextInt())
+	
+	if (userInput.hasNextInt())
+	{
+		int numberEntered = userInput.nextInt();
+		
+		System.out.println("You entered" + numberEntered);
+		int numEnteredTime2= numberEntered + numberEntered;
+		System.out.printline(NumberEntered + "=" + numberEntered + "=" + numEnteredTime2);
+	}else {
+		System.out.printLn("enter an interger next time");
+		
+	}
+}
+}
