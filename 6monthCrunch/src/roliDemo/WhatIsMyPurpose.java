e74d792678f1b3abeb8cb2b20ab97ce1d3f701d8
Liang <Liang@DESKTOP-H2H9IM4.home>
2018-05-10 20:56:55 +0200

    20:56

--- 6monthCrunch/src/roliDemo/WhatIsMyPurpose.java
+++ 6monthCrunch/src/roliDemo/WhatIsMyPurpose.java
@@ -7,13 +7,15 @@ public class WhatIsMyPurpose {
 		// TODO Auto-generated method stub
 
 		Scanner input= new Scanner (System.in);
+		
 		WhatIsMyPurpose testObject = new WhatIsMyPurpose();
+	
 		
 		System.out.println("What´s my purpose");
 		String name = input.nextLine();
 		
-		System.out.println(name);
-		WhatIsMyPurpose testObject2 = new WhatIsMyPurpose();
+		
+		
 		Scanner input2= new Scanner (System.in);
 		System.out.println("Oh no");
 		String name2 = input2.nextLine();
