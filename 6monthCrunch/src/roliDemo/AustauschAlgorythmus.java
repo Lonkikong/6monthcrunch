326219bcedf5438fd0ad06179e37ed2bb6673f54
Lonkikong <38979905+Lonkikong@users.noreply.github.com>
2018-05-14 11:49:46 +0200

    Update and rename AustauschAlgorythmus to AustauschAlgorythmus.java

---
+++ 6monthCrunch/src/roliDemo/AustauschAlgorythmus.java
@@ -0,0 +1,15 @@
+package roliDemo;
+
+
+
+public class AustauschAlgorythmus {
+
+
+  //AUFGABE: Erstelle eine Schleife mit der du jeden VOKAL klein schreibst (austauscht mit einer kleinen Version), jeden KONSUNANT groß schreibst (austauscht mit einer großen Version) und jeder doppelte Buchstabe austauscht mit "**" (doppelte Sterchen)
+  //Verwende hierfür eine while loop. Du wirst mehrere IF Bedingungen benötigen, kannst aber auch mit Switches arbeiten
+  //Führe diese Klasse mit einer anderen main klasse aus oder erstelle eine main funktion hier. Deine Wahl
+  
+  private String input = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";
+  
+
+}
